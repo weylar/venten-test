e738885995ea2a0bd6078f4281ad51977883ae00
weylar <idrisaminu861@gmail.com>
2020-02-06 10:55:45 +0100

    Complete the main UI

--- app/src/main/java/com/idris/aminu/android/util/Color.kt
+++ app/src/main/java/com/idris/aminu/android/util/Color.kt
@@ -1,15 +1,15 @@
 package com.idris.aminu.android.util
 
 enum class Color (var color: String) {
-    GREEN("green"),
-    VIOLET("yellow"),
-    YELLOW("yellow"),
-    BLUE("blue"),
-    TEAL("teal"),
-    MAROON("maroon"),
-    RED("red"),
-    AQUAMARINE("aquamarine"),
-    ORANGE("orange"),
-    MAUV("mauv")
+    GREEN("Green"),
+    VIOLET("Violet"),
+    YELLOW("Yellow"),
+    BLUE("Blue"),
+    TEAL("Teal"),
+    MAROON("Maroon"),
+    RED("Red"),
+    AQUAMARINE("Aquamarine"),
+    ORANGE("Orange"),
+    MAUV("Mauv")
 
 }
